74301a9d9514f6246a25a9a37c5f4371f9374760
Boudje412 <boudjemai.remi@gmail.com>
2022-04-08 11:03:35 +0200

    Update player.ts

--- player.ts
+++ player.ts
@@ -1,10 +1,24 @@
+import {Cup} from "./cup"
+import {Dice} from "./dice"
 export class Player{
     private _name:string;
     private _score:number;
-    play(cup):void{
-
+    constructor(name:string)
+    {
+        this._name = name;
+        this._score = 0;
+    }
+    get name():string{
+        return this._name;
+    }
+    play(cup:Cup):void{
+        let dice=new Dice(6);
+        let res=cup.throw(dice);
+        res.forEach(a=>{
+            this._score=this._score+a
+        })
     }
     printScore():void{
-
+        console.log(this._name+' a pour score:'+this._score)
     }
 }
